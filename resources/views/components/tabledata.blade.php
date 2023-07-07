8332119c1752c1fa485e22b84c9495a8793956fc
venom-10 <arohit0102@gmail.com>
2023-07-07 13:00:54 +0530

    updated design

--- resources/views/components/tabledata.blade.php
+++ resources/views/components/tabledata.blade.php
@@ -7,9 +7,9 @@
     <td class="px-6 py-[10px]">{{ $data->state }}</td>
     <td class="px-6 py-[10px]">{{ $data->address }}</td>
     <td class="px-6 py-[10px]">{{ $data->dob }}</td>
-    <td class="px-6 py-[10px] w-full flex items-center justify-center mt-[10px]">
+    <td class="px-6 py-[10px] w-full flex gap-4 items-center justify-center mt-[10px]">
         <a href='/edit?id={{$data->id}}'
-            class="text-gray-400 font-semibold hover:text-green-600">Edit</a>
+            class="text-gray-400 font-semibold hover:text-green-500">Edit</a>
         <button id="dropdownDefaultButton" data-dropdown-toggle="delete_Dropdown{{$data->id}}"
             class="relative font-semibold hover:text-red-600 ml-2"
             type="button">Delete</button>
@@ -17,8 +17,6 @@
             class="hidden text-white bg-red-700 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
             <a href='/delete?id={{$data->id}}' class="text-gray-50 hover:text-gray-200">Are you Sure</a>
         </div>
-        <a href='/'
-            class=" ml-2 font-semibold hover:text-green-700">Add photo</a>
     </td>
     <td class="px-6 py-[10px] text-center"> <img class="w-10 h-10 rounded-full" src="{{ $data->imagepath ? asset('storage/uploads/'.$data->imagepath) : asset('assets/images/default.jpg') }}" alt="user photo"> </td>
 </tr>
--- resources/views/partials/manipulateNav.blade.php
+++ resources/views/partials/manipulateNav.blade.php
@@ -39,4 +39,4 @@
         Select</button>
 </form>
 <a type="button" href='./user/createform'
-    class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add</a>
+    class="text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add</a>
